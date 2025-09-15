463c458b554f04aaed1e5b348d4e1dcfc78bd943
Arshad Khan <muhammadarhsad0910@gmail.com>
2025-09-15 23:10:19 +0100

    update releaes

--- dist/utils/Logger.js
+++ dist/utils/Logger.js
@@ -6,15 +6,10 @@ Object.defineProperty(exports, "__esModule", {
 exports.Logger = void 0;
 var _chalk = _interopRequireDefault(require("chalk"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-// src/utils/Logger.js
-
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); } // src/utils/Logger.js
 class Logger {
-  static LEVELS = {
-    error: 0,
-    warn: 1,
-    info: 2,
-    verbose: 3
-  };
   constructor({
     level = 'info',
     showTimestamps = true,
@@ -86,3 +81,9 @@ class Logger {
   }
 }
 exports.Logger = Logger;
+_defineProperty(Logger, "LEVELS", {
+  error: 0,
+  warn: 1,
+  info: 2,
+  verbose: 3
+});
